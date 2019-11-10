bdea5fb0d6e3bc22c376f6af451c4218fce6bb88
Babar Waheed <babar.waheed@gmail.com>
2019-11-10 21:52:30 +1100

    Rendering HTML by Node.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>HTML page by Node.js</title></head>');
+    res.write('<body><h1>Rendering HTML by Node.js</h1></body></html>');
+    res.end();
 }); 
 
 server.listen(3000);
