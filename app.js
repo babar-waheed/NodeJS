dcac2254fbf75fafdfaefaa6704085a17b9d0fef
Babar Waheed <babar.waheed@gmail.com>
2019-11-16 20:30:55 +1100

    Cookies & Sessions using EXPRESS-SESSION CONNECT-MONGODB-SESSION

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const USER_PASSWORD = require('./utils/password');
+
+const MONGODB_URI = `mongodb+srv://${USER_PASSWORD}@nodejs-47ykt.mongodb.net/shop`
+
 const app = express();
+const store = new mongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,15 +27,26 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('5dcfa412ed44ca4fe277c4a1')
+  if(!req.session.user) return next();
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
-});
+    .catch(err => {
+      console.log(err);
+    })
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -35,9 +55,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-.connect(
-  `mongodb+srv://${USER_PASSWORD}@nodejs-47ykt.mongodb.net/shop?retryWrites=true&w=majority`
-  )
+.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
